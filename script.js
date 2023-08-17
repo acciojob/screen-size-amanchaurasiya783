c9068c87376c6132758cfd6e35254d15dae7251a
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-17 09:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let h1 = document.getElementsByTagName('h1');
+let width = window.innerWidth;
+let height = window.innerHeight;
+h1[0].innerHTML = 'Width: ' + width + ' and Height: ' + height;
